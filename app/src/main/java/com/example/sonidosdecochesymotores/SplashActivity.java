ec86f633f7c55261d0a77039bdafe03c6c3b4d9a
gurzelai <gorkaurzelai2000@gmail.com>
2021-03-12 00:19:10 +0100

    V1.0 Primera versión

---
+++ app/src/main/java/com/example/sonidosdecochesymotores/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.sonidosdecochesymotores;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
